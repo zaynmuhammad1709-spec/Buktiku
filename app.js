2b32c8cca42c77403c2000834f1b8c9f98d0d340
zaynmuhammad1709-spec <zaynmuhammad1709@gmail.com>
2026-02-18 12:46:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // FIREBASE CONFIG
 var firebaseConfig = {
-  apiKey: "ISI_API_KEY",
-  authDomain: "ISI_PROJECT.firebaseapp.com",
-  projectId: "ISI_PROJECT",
-  storageBucket: "ISI_PROJECT.appspot.com",
-  messagingSenderId: "ISI_SENDER_ID",
-  appId: "ISI_APP_ID"
+  apiKey: "AIzaSyBIaTx9x1_yz5N6t0uBp2eTc7tIBhMLRQA",
+  authDomain: "buktiku-6e36b.firebaseapp.com",
+  projectId: "buktiku-6e36b",
+  storageBucket: "buktiku-6e36b.firebasestorage.app",
+  messagingSenderId: "864927339878",
+  appId: "1:864927339878:web:e11a53ac31e63c098c64a7",
+  measurementId: "G-QBTSPKXLND"
 };
 
 firebase.initializeApp(firebaseConfig);
