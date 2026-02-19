e2c161b159e4d09635f51a5b97b07e819c8a0766
zaynmuhammad1709-spec <zaynmuhammad1709@gmail.com>
2026-02-19 13:33:04 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://xrvvwyrvjwloxxvcurev.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhydnZ3eXJ2andsb3h4dmN1cmV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NzEwMTksImV4cCI6MjA4NzA0NzAxOX0.ZgC-oi8RaqKK4tkzPpECO_fOUUMUqFpl0p6oxkXMJK8";
-
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
