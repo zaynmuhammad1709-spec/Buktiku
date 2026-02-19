28bfd13d2dc50d90fd09882cee852e3ed412227b
zaynmuhammad1709-spec <zaynmuhammad1709@gmail.com>
2026-02-19 13:12:04 +0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-const supabase = window.supabase.createClient(
-  "https://xrvvwyrvjwloxxvcurev.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhydnZ3eXJ2andsb3h4dmN1cmV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NzEwMTksImV4cCI6MjA4NzA0NzAxOX0.ZgC-oi8RaqKK4tkzPpECO_fOUUMUqFpl0p6oxkXMJK8"
-);
